e1e5ce5958ac3394e378991265cab96578196831
Dan Krupsky <dan.krupsky@gmail.com>
2022-07-21 20:18:38 +0300

    mongo db module

--- index.js
+++ index.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/sp-guestbook";
+
 app.get('/', (req, res) => {
+  // DB Connection testing
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.createCollection("customers", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+  });
+
+  
   res.send('Hello World!')
+  console.log("done");
 })
 
 app.listen(port, () => {
