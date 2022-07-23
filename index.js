fd67b60ac682ed813f9f39ee0e17125462dd49a6
Dan Krupsky <dan.krupsky@gmail.com>
2022-07-23 20:37:49 +0300

    write-read from mongodb

--- index.js
+++ index.js
@@ -6,22 +6,62 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/sp-guestbook";
 
 app.get('/', (req, res) => {
-  // DB Connection testing
+  // DB insert
   MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    dbo.createCollection("customers", function(err, res) {
+    var query = { name: "John" };
+    dbo.collection("customers").find(query).toArray(function(err, result) {
       if (err) throw err;
-      console.log("Collection created!");
+      console.log(result);
       db.close();
+      res.send('Hello World!' + '\n' + result[0].name + '\n' + result[0].message);
+    console.log("done");
     });
   });
 
   
-  res.send('Hello World!')
-  console.log("done");
+  
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// Create collection
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   dbo.createCollection("customers", function(err, res) {
+//     if (err) throw err;
+//     console.log("Collection created!");
+//     db.close();
+//   });
+// });
+
+
+// Insert message
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   var myobj = { name: "John", message: "Test-Test" };
+//   dbo.collection("customers").insertOne(myobj, function(err, res) {
+//     if (err) throw err;
+//     console.log("1 document inserted");
+//     db.close();
+//   });
+// });
+
+
+// Read
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db("mydb");
+//   var query = { address: "Park Lane 38" };
+//   dbo.collection("customers").find(query).toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     db.close();
+//   });
+// });
