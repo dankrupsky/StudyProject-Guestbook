eb5de5d0fa128c5f39cad5ac5199913ad9bd264d
Dan Krupsky <dan.krupsky@gmail.com>
2022-08-08 19:16:38 +0300

    renaming
    service added

---
+++ services/CommentService.js
@@ -0,0 +1,26 @@
+const Comment = require('../models/Comment');
+
+module.exports = class Comment{
+    static async gelAllComments() {
+        try {
+            const allComments = await Comment.find();
+            return allComments;
+        } catch (error) {
+            console.log(`Could not find any comments ${error}`)
+        }
+    }
+
+    static async addComment(data) {
+        try {
+            const newComment = {
+                name: data.username,
+                text: data.text,
+                ip: data.ip,
+            }
+            const response = await new Comment(this.addComment).save();
+            return response;
+        } catch (error) {
+            console.log(`Failed to add new comment ${error}`)
+        }
+    }
+};
